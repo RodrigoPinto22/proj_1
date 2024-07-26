ae2ea6d8b1c2ae1bf75029356bdb0d87e4bff2c3
Rodrigo Pinto <rodrigomotap@gmail.com>
2024-07-26 11:47:48 +0100

    Add new files

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+tasks = []
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task = request.form.get('task')
+        print(task)
+        tasks.append(task)
+    return render_template("index.html")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/task_list")
+def task_list():
+    print(tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
